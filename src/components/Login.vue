98df3a8ed91d4eaf04c2c8000acb074f41b9a1f2
wangliang <745837850@qq.com>
2020-10-29 14:25:06 +0800

    删除无用代码&配置登录路由和组件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>
+        <h1>登录</h1>
+    </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+    name:'Login'    
 }
 </script>
-
 <style>
 
 </style>
